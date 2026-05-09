e7da540261e462e6c64167d0baf6e6125844def5
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-09 13:14:18 -0700

    Displayed multiple movie recommendation cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import Mc from "./components/Mc";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-slate-900 flex justify-center items-center"><Mc/></div>
+    <div className="min-h-screen bg-slate-900 flex justify-center items-center">
+      <h1 className="text-white text-5xl mb-10 text-center">
+        🎬 Movie Recommendations
+      </h1>
+
+      <div className="flex-gap-6">
+        <Mc/>
+        <Mc/>
+        <Mc/>
+      </div>
+
+    </div>
   )
 }
 
