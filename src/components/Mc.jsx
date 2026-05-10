62a9d09197e4ac45c3cd8ca123cf0e13d6727f73
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-10 13:09:25 -0700

    Added props support to MovieCard component

--- src/components/Mc.jsx
+++ src/components/Mc.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-const Mc = () => {
+const Mc = (props) => {
   return (
     <div className="bg-slate-800 p-4 rounded-xl w-64">
-        <img src="https://via.placeholder.com/250x350"
+        <img src={props.image}
         alt="movie"
         className="rounded-lg"></img>
 
         <h2 className="text-white text-xl mt-3">
-            Interstellar
+            {props.title}
         </h2>
 
-       <p className="text-gray-400">⭐ 8.7</p> 
+       <p className="text-gray-400">⭐ {props.rating}</p> 
 
-       <button className="bg-blue-500 px-4 py-2 rounded-lg mt-3 text-white">
+       <button className="bg-blue-500 px-4 py-2 rounded-lg mt-3 text-white p-4  w-64 hover:scale-105 transition">
         Watch Now</button>  
       
     </div>
