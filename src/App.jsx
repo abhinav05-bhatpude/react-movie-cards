edf17d264a266f4bd1c0d4ebef049be41add52e4
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-10 13:26:59 -0700

    Organized movie information into structured array data

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,56 @@
-import React from 'react'
+import React from "react";
 import Mc from "./components/Mc";
 
 const App = () => {
+
+  const movies = [
+    {
+      title: "Interstellar",
+      rating: "8.7",
+      genre: "Sci-Fi",
+      image:
+        "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
+    },
+
+    {
+      title: "Inception",
+      rating: "8.8",
+      genre: "Action",
+      image:
+        "https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg",
+    },
+
+    {
+      title: "Batman",
+      rating: "8.4",
+      genre: "Action",
+      image:
+        "https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
+    },
+  ];
+
   return (
-    <div className="min-h-screen bg-slate-900 flex justify-center items-center">
+    <div className="min-h-screen bg-slate-900 p-10">
+
       <h1 className="text-white text-5xl mb-10 text-center">
         🎬 Movie Recommendations
       </h1>
 
-      <div className="flex-gap-6">
-        <div className="bg-slate-800 p-4 rounded-xl w-64 hover:scale-105 transition duration-300">
+      <div className="flex gap-6 flex-wrap justify-center">
+
+        {movies.map((movie) => (
           <Mc
-          title="Interstellar"
-          rating="8.7"
-          image="https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg"
-          genre="sci-fi"
+            title={movie.title}
+            rating={movie.rating}
+            genre={movie.genre}
+            image={movie.image}
           />
-        </div>
-         <div className="bg-slate-800 p-4 rounded-xl w-64 hover:scale-105 transition duration-300">
-          <Mc
-          title="Inception"
-          rating="8.8"
-          image="https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"
-          genre="sci-fi"
-          />
-        </div>
-         <div className="bg-slate-800 p-4 rounded-xl w-64 hover:scale-105 transition duration-300">
-          <Mc/>
-        </div>
+        ))}
+
       </div>
 
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
