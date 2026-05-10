dbe896d70579779748504ae148839e16616f9afc
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-10 13:31:56 -0700

    Enhanced movie card design with improved hover effects

--- src/components/Mc.jsx
+++ src/components/Mc.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Mc = (props) => {
   return (
-    <div className="bg-slate-800 p-4 rounded-xl w-64">
+    <div className="bg-slate-800 p-4 rounded-2xl w-64 hover:scale-105 hover:shadow-2xl transition duration-300">
         <img src={props.image}
         alt="movie"
         className="rounded-lg"></img>
