9ec75d21fe68af334d0c1c8355d6c205181df4b2
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-10 13:12:01 -0700

    Passed dynamic movie data through props

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,18 @@ const App = () => {
 
       <div className="flex-gap-6">
         <div className="bg-slate-800 p-4 rounded-xl w-64 hover:scale-105 transition duration-300">
-          <Mc/>
+          <Mc
+          title="Interstellar"
+          rating="8.7"
+          image="https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg"
+          />
         </div>
          <div className="bg-slate-800 p-4 rounded-xl w-64 hover:scale-105 transition duration-300">
-          <Mc/>
+          <Mc
+          title="Inception"
+          rating="8.8"
+          image="https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg"
+          />
         </div>
          <div className="bg-slate-800 p-4 rounded-xl w-64 hover:scale-105 transition duration-300">
           <Mc/>
